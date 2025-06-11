99ff619dbff5c3a6eb73cef25304192aa5f65d4a
Sonya <sonenkkaa@gmail.com>
2025-06-11 22:29:32 +0300

    update text styles for main menu

--- src/components/Quiz/Question.tsx
+++ src/components/Quiz/Question.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { QuestionType } from '../../../lib/types';
+import { QuestionType } from '../../../lib/types.ts';
 import styles from './styles.module.css';
 
 export default function Question({
