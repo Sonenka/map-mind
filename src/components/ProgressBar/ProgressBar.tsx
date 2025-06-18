0ba15d0dd3af6256c5de4dd3c3e805b4fc62dc62
Sonya <sonenkkaa@gmail.com>
2025-06-18 23:13:05 +0300

    add back buttons

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import styles from './ProgressBar.module.css';
+import Link from 'next/link';
 
 export default function ProgressBar({
   current,
@@ -12,14 +13,22 @@ export default function ProgressBar({
   const percentage = Math.round((current / total) * 100);
 
   return (
-    <div className={styles.progressContainer}>
-      <div 
-        className={styles.progressBar}
-        style={{ width: `${percentage}%` }}
-      />
-      <span className={styles.progressText}>
-        Вопрос {current} из {total} ({percentage}%)
-      </span>
+    <div className={styles.progressWrapper}>
+      <div className={styles.progressContainer}>
+        <div 
+          className={styles.progressBar}
+          style={{ width: `${percentage}%` }}
+        />
+        <span className={styles.progressText}>
+          Вопрос {current} из {total} ({percentage}%)
+        </span>
+      </div>
+      <Link
+          href="/single"
+          className={styles.progressLink}
+        >
+          ✕
+      </Link>
     </div>
   );
 }
